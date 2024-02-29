d2ef1da1686abe1dd19f411456dc46a9e8e50b49
Malik Najeeb Ul Habib <maliknajeeb113@gmail.com>
2024-03-01 00:57:12 +0530

    add: my services

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,4 +1,6 @@
 import hero from "../assets/hero.jpeg";
+import { PiHandWaving } from "react-icons/pi";
+
 
 import {
   FaArrowRight,
@@ -30,7 +32,7 @@ const Body = () => {
         <div className="flex flex-col gap-6" id="hero-left">
           <div className=" flex flex-col gap-6">
             <h1
-              className="!leading-[1.2] text-3xl md:text-6xl font-medium "
+              className="!leading-[1.2] text-3xl md:text-5xl font-medium "
               font-medium
             >
               Hi, I'm Avni Garg. <br /> UX/UI Designer based in Bangalore.
@@ -39,31 +41,31 @@ const Body = () => {
               I focus on minimalism and easy-to-understand information.
             </span>
           </div>
-          <div className="flex flex-col md:flex-row justify-start gap-6">
+          <div className="flex flex-col md:flex-row justify-start gap-6 text-[rgb(126,126,126)]">
             <div
-              className="flex justify-center rounded-xl hover:shadow-neu-i items-center shadow-neu w-56 font-medium text-xl h-16 gap-3 cursor-pointer"
+              className="flex justify-center rounded-2xl hover:shadow-neu-i items-center shadow-neu w-56 font-medium text-lg h-16 gap-3 cursor-pointer "
               id="get-in-touch"
             >
               <FaArrowRight />
               <span>Get in touch</span>
             </div>
-            <div id="hero-icons" className="flex flex-row gap-4 items-center">
+            <div id="hero-icons" className="flex flex-row gap-4 items-center ">
               <Link to={"https://www.behance.net/avniigarg"} target="_blank">
-                <div className="shadow-neu p-3 hover:scale-105 duration-300 rounded-full hover:shadow-neu-i">
-                  <FaBehance className="w-5 h-5 font-semibold" />
+                <div className="shadow-neu p-3 hover:scale-105 duration-500 rounded-full hover:shadow-neu-i">
+                  <FaBehance className="w-5 h-5 font-semibold " />
                 </div>
               </Link>
               <Link
                 to={"https://www.linkedin.com/in/garg-avni/"}
                 target="_blank"
               >
-                <div className="shadow-neu p-3 hover:scale-105 duration-300 rounded-full hover:shadow-neu-i">
+                <div className="shadow-neu p-3 hover:scale-105 duration-500 rounded-full hover:shadow-neu-i">
                   <FaLinkedinIn className="w-5 h-5" />
                 </div>
               </Link>
               <Link to={"https://twitter.com/avniigarg"} target="_blank">
                 {" "}
-                <div className="shadow-neu p-3 hover:scale-105 duration-300 rounded-full hover:shadow-neu-i">
+                <div className="shadow-neu p-3 hover:scale-105 duration-500 rounded-full hover:shadow-neu-i">
                   <FaXTwitter className="w-5 h-5" />
                 </div>
               </Link>
@@ -71,7 +73,7 @@ const Body = () => {
                 to={"https://www.instagram.com/avnigarg.ux/"}
                 target="_blank"
               >
-                <div className="shadow-neu p-3 hover:scale-105 duration-300 rounded-full hover:shadow-neu-i">
+                <div className="shadow-neu p-3 hover:scale-105 duration-500 rounded-full hover:shadow-neu-i">
                   <FaInstagram className="w-5 h-5" />
                 </div>
               </Link>
@@ -81,10 +83,10 @@ const Body = () => {
       </div>
       {/* hero section end */}
 
-      {/* body section */}
-      <div className="container flex pt-28 justify-between items-end">
-        <div className="shadow-neu rounded-3xl p-10 w-4/6">
-          <div className="text-5xl font-medium">About Me 👋🏻</div>
+      {/* about section */}
+      <div className="container flex flex-col md:flex-row pt-32 justify-between md:items-end gap-8">
+        <div className="shadow-neu rounded-3xl p-10 md:w-4/6">
+          <div className="text-4xl font-medium flex gap-3">About Me <span><PiHandWaving/></span></div>
           <div className="text-[rgb(156,156,156)]  md:text-lg font-thin pt-4">
             Hello there! Recent graduate with a B.Tech, specialising in simple
             and sleek design. I create fun and user-friendly interfaces – even
@@ -95,13 +97,29 @@ const Body = () => {
         </div>
 
         <div
-              className="flex justify-center rounded-xl hover:shadow-neu-i items-center shadow-neu w-56 font-medium text-xl h-16 gap-3 cursor-pointer"
+              className="flex justify-center rounded-2xl hover:shadow-neu-i items-center shadow-neu w-56 font-medium text-lg h-16 gap-3 cursor-pointer text-[rgb(126,126,126)]"
               id="get-in-touch"
             >
               <FaArrowRight />
-              <span>Get in touch</span>
+              <span>More About Me</span>
             </div>
       </div>
+      {/* about section end */}
+
+      {/* my Services */}
+      <div className="container flex flex-col lg:flex-row pt-32">
+        <h1 className="text-4xl font-medium">My Services</h1>
+
+        <div className="flex flex-col md:flex-row" id="cards">
+          <div className="shadow-neu"></div>
+
+
+        </div>
+
+
+      </div>
+      {/* my services end */}
+
     </main>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   return (
     <nav className={`${isNavOpen ? "h-screen" : ""} w-screen text-[rgb(85,85,85)] font-poppins fixed top-0 bg-[rgb(241,241,241)] z-10`}>
       <div className="container flex px-6 md:px-0 md:mx-auto py-4 items-center justify-between">
-        <div className="font-semibold text-3xl" id="Logo">
+        <div className="font-medium text-3xl" id="Logo">
           <Link to={"/"}>Avni.</Link>
         </div>
         <div className={`${isNavOpen?"shadow-neu-i":"shadow-neu"} rounded-full p-3 z-10`} id="Hamburger">
@@ -30,26 +30,26 @@ const Navbar = () => {
       >
         <ul className="flex flex-col gap-10 items-center p-10">
           <Link to={"/"} onClick={toggleNav} >
-            <li className="shadow-neu px-20 py-10 rounded-xl text-xl cursor-pointer hover:shadow-neu-i hover:scale-105 duration-300">Home</li>
+            <li className="shadow-neu px-20 py-10 rounded-xl text-xl cursor-pointer hover:shadow-neu-i hover:scale-105 duration-500">Home</li>
           </Link>
           <Link to={"/about"} onClick={toggleNav}>
-            <li className="shadow-neu px-20 py-10 rounded-xl text-xl cursor-pointer hover:shadow-neu-i hover:scale-105 duration-300">About</li>
+            <li className="shadow-neu px-20 py-10 rounded-xl text-xl cursor-pointer hover:shadow-neu-i hover:scale-105 duration-500">About</li>
           </Link>
           <Link to={"/contact"} onClick={toggleNav}>
-            <li className="shadow-neu px-20 py-10 rounded-xl text-xl cursor-pointer hover:shadow-neu-i hover:scale-105 duration-300">Contact</li>
+            <li className="shadow-neu px-20 py-10 rounded-xl text-xl cursor-pointer hover:shadow-neu-i hover:scale-105 duration-500">Contact</li>
           </Link>
         </ul>
         <div className="flex flex-row items-center justify-center gap-x-5">
-            <Link to={"https://www.behance.net/avniigarg"} target="_blank"><div className="shadow-neu p-3 hover:scale-105 duration-300 rounded-full hover:shadow-neu-i">
+            <Link to={"https://www.behance.net/avniigarg"} target="_blank"><div className="shadow-neu p-3 hover:scale-105 duration-500 rounded-full hover:shadow-neu-i">
                 <FaBehance className="w-5 h-5 font-semibold"/>
             </div></Link>
-            <Link to={"https://www.linkedin.com/in/garg-avni/"} target="_blank"><div className="shadow-neu p-3 hover:scale-105 duration-300 rounded-full hover:shadow-neu-i">
+            <Link to={"https://www.linkedin.com/in/garg-avni/"} target="_blank"><div className="shadow-neu p-3 hover:scale-105 duration-500 rounded-full hover:shadow-neu-i">
                 <FaLinkedinIn className="w-5 h-5"/>
             </div></Link>
-            <Link to={"https://twitter.com/avniigarg"} target="_blank"> <div className="shadow-neu p-3 hover:scale-105 duration-300 rounded-full hover:shadow-neu-i">
+            <Link to={"https://twitter.com/avniigarg"} target="_blank"> <div className="shadow-neu p-3 hover:scale-105 duration-500 rounded-full hover:shadow-neu-i">
                 <FaXTwitter className="w-5 h-5"/>
             </div></Link>
-            <Link to={"https://www.instagram.com/avnigarg.ux/"} target="_blank"><div className="shadow-neu p-3 hover:scale-105 duration-300 rounded-full hover:shadow-neu-i">
+            <Link to={"https://www.instagram.com/avnigarg.ux/"} target="_blank"><div className="shadow-neu p-3 hover:scale-105 duration-500 rounded-full hover:shadow-neu-i">
                 <FaInstagram className="w-5 h-5"/>
             </div></Link>
 
