092f4bafd14ea54f2736df570832391ae5202763
maliknajeeb113 <maliknajeeb113@gmail.com>
2025-03-16 23:59:03 +0530

    update: removed the branshark page from home

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,5 +1,5 @@
 import salesken from "../assets/salesken/salesken.jpeg";
-import brandsharkCover from "../assets/brandshark/brandsharkCover.png";
+// import brandsharkCover from "../assets/brandshark/brandsharkCover.png";
 import tanishqCover from "../assets/portfolio-project/tanishqCover.png";
 import bliveCover from "../assets/blive/bliveCover.png";
 
@@ -26,11 +26,11 @@ export const jobsData = [
     desc: "Created a user-friendly website tailored to clients needs, providing a centralized platform to showcase their work, skills, and accomplishments efficiently, facilitating effective presentation of their portfolio.",
     linkTo: "/portfolio-project",
   },
-  {
-    imgLink: brandsharkCover,
-    jobTitle: "Marketing Website Redesign",
-    companyName: "Marketing Website Redesign",
-    desc: "Crafted a sleek website showcasing Brand Shark's portfolio and expertise in video production, capturing attention and engaging potential clients effectively.",
-    linkTo: "/brandshark",
-  },
+  // {
+  //   imgLink: brandsharkCover,
+  //   jobTitle: "Marketing Website Redesign",
+  //   companyName: "Marketing Website Redesign",
+  //   desc: "Crafted a sleek website showcasing Brand Shark's portfolio and expertise in video production, capturing attention and engaging potential clients effectively.",
+  //   linkTo: "/brandshark",
+  // },
 ];
